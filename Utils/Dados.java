12dfe658c1fd67ab843e640719242754a9904f21
dinafleck <dinararfleck@gmail.com>
2025-06-14 16:09:50 -0300

    03_método para ler e escrever dados

--- Utils/Dados.java
+++ Utils/Dados.java
@@ -1,5 +1,36 @@
 package Utils;
 
+import java.io.*;
+
 public class Dados {
-    
+
+    public static void escrever(String caminho, String nomeArquivo, String conteudo) throws IOException {
+        if (new File(caminho).mkdirs()) {
+            System.out.println("Pasta criada");
+        } else {
+            System.out.println("Erro ao criar pasta");
+        }
+
+        File file = new File(caminho + "/" + nomeArquivo);
+
+        if (file.createNewFile()) {
+            System.out.println("Arquivo criado");
+        } else {
+            System.out.println("Arquivo já existe");
+        }
+
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write(conteudo);
+        }
+    }
+
+    public static String ler(String caminho, String nomeArquivo) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho + "/" + nomeArquivo))) {
+            return br.readLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
