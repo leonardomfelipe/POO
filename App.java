97971b51428c5a174c8ae595befd511d00eee140
dinafleck <dinararfleck@gmail.com>
2025-06-14 17:27:35 -0300

    04_Main para rodar e encerrar o programa

--- App.java
+++ App.java
@@ -1,5 +1,25 @@
+import Sistema.Funcao;
+import Sistema.Funcoes;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Insira a sua opção:");
+            System.out.println("sair - para encerrar o programa.");
+
+            String enter = scanner.nextLine();
+
+            Funcao funcao = Funcoes.encontrarFuncao(enter);
+
+            if (funcao != null) {
+                funcao.executar();
+            } else {
+                System.out.println("Opção inválida.");
+            }
+        }
     }
 }
---
+++ Sistema/Funcoes.java
@@ -0,0 +1,32 @@
+package Sistema;
+
+public enum Funcoes {
+    ENCERRAR(new EncerrarPrograma(), "sair");
+
+    private Funcao funcao;
+    private String comando;
+
+    Funcoes(Funcao funcao, String comando) {
+        this.funcao = funcao;
+        this.comando = comando;
+    }
+
+    public static Funcao encontrarFuncao(String comando) {
+        for (Funcoes f : Funcoes.values()) {
+            if (f.getComando().equals(comando)) {
+                return f.getFuncao();
+            }
+        }
+        return null;
+    }
+
+    public Funcao getFuncao() {
+        return this.funcao;
+    }
+
+    public String getComando() {
+        return this.comando;
+    }
+
+
+}
