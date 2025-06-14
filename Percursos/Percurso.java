a3634a988fc0ff8e05aaa1965c83143d52184c14
dinafleck <dinararfleck@gmail.com>
2025-06-14 15:37:22 -0300

    cadastro local e percurso

---
+++ Percursos/Percurso.java
@@ -0,0 +1,15 @@
+package Percursos;
+
+public class Percurso {
+    private int codigo;
+    private String nome;
+    private double distancia;
+    private String cidade;
+
+    public Percurso(String nome, double distancia, String cidade) {
+        this.codigo = 0;
+        this.nome = nome;
+        this.distancia = distancia;
+        this.cidade = cidade;
+    }
+}
