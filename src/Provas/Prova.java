55793a513623969bf55c02e0ec13175006bdea5b
jesalvatori <jessica.salvatori@icloud.com>
2025-06-15 00:41:49 -0300

    Merge remote-tracking branch 'origin/main'

--- src/Provas/Prova.java
+++ src/Provas/Prova.java
@@ -1,5 +1,13 @@
 package Provas;
 
+import Atletas.Atleta;
+import Percursos.Local;
+import Percursos.Percurso;
+
 public class Prova {
-    
+    private int codigo;
+    private TipoProva tipo;
+    private Percurso percurso;
+    private Local local;
+    private Atleta atleta;
 }
